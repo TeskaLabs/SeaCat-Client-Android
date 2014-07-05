18f4492d4cb8d3f7b3f9e25b8bdfadd10c436575
Ales Teska <ales.teska@exidius.com>
2014-07-05 12:17:01 +0200

    Creating test and demo projects.

---
+++ test/src/mobi/seacat/client/android/test/SeaCatClientTest.java
@@ -0,0 +1,71 @@
+package mobi.seacat.client.android.test;
+
+import android.test.AndroidTestCase;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import mobi.seacat.client.SeaCatClient;
+
+public class SeaCatClientTest extends AndroidTestCase
+{
+	
+	public void testConnect() throws IOException
+	{
+		SeaCatClient.connect();
+	}
+
+	
+	public void testDoubleConnect() throws IOException
+	{
+		SeaCatClient.connect();
+		SeaCatClient.connect();
+	}
+
+	
+	public void testDisconnect() throws IOException
+	{
+		SeaCatClient.disconnect();
+	}
+
+	
+	public void testDoubleDisconnect() throws IOException
+	{
+		SeaCatClient.disconnect();
+		SeaCatClient.disconnect();
+	}
+
+	public void testPing() throws Exception
+	{
+		SeaCatClient.ping(1000);
+	}
+
+
+	public void testGETHelloWorld() throws IOException
+	{
+		URL url = new URL("https://testhost.seacat/get/hello_world");
+		HttpURLConnection conn = SeaCatClient.open(url);
+
+		InputStream is = conn.getInputStream();
+		assertNotNull(is);
+
+		String line;
+		String result = "";
+	
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		while ((line = rd.readLine()) != null)
+		{
+			result += line;
+		}
+		rd.close();
+
+		assertEquals("Hello World!", result);
+		
+		is.close();
+	}
+
+}
