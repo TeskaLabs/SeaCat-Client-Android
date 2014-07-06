6fb87e1d62d90528c1273cb0e768218bcdfcf686
Ales Teska <ales.teska@exidius.com>
2014-07-06 14:16:47 +0200

    Improving automated testing script.

--- run-test.sh
+++ run-test.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
 TEST_OUT=/tmp/mobi.seacat.client.android.test.out
+TEST_COUNTER=0
 
 while true
 do
+	COUNTER=$[$COUNTER +1]
+	echo -n "Test round $COUNTER started at "
+	date
+
 	rm -f ${TEST_OUT}
 	~/Android/sdk/platform-tools/adb shell am instrument -w mobi.seacat.client.android.test/android.test.InstrumentationTestRunner | tee ${TEST_OUT}
 
 	RESULT=`cat ${TEST_OUT} | grep -e "^OK " | cut -d" " -f1`
 	if [ "$RESULT" != "OK" ];
 	then
-		break       	   #Abandon the loop.
+		break
 	fi
 done
 
