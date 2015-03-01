96471b9f03732fc31122fdc0ed2da817dad9f03b
Ales Teska <ales.teska@exidius.com>
2015-03-01 20:00:38 +0100

    Proceeding with functional enablement and testing.

--- client/src/main/jni/build.sh
+++ client/src/main/jni/build.sh
@@ -6,7 +6,7 @@ JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 JAVAC=javac
 
 # Clean everything first
-rm -f mobi_seacat_client_internal_*.h
+#rm -f mobi_seacat_client_internal_*.h
 rm -fr ../obj ../libs/armeabi-v7a ../libs/armeabi ../libs/x86
 
 # Prepare compiled class
@@ -17,4 +17,4 @@ rm -fr ../obj ../libs/armeabi-v7a ../libs/armeabi ../libs/x86
 #javah -d . -classpath ~/Android/sdk/platforms/android-19/android.jar:../../../build/jni/classes mobi.seacat.client.core.seacatcc
 
 # Compile Android JNI
-#${ANDROID_NDK}/ndk-build
+${ANDROID_NDK}/ndk-build
--- demo/src/main/java/mobi/seacat/AndroidDemoApp/MainActivity.java
+++ demo/src/main/java/mobi/seacat/AndroidDemoApp/MainActivity.java
@@ -1,44 +1,126 @@
 package mobi.seacat.AndroidDemoApp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.util.Log;
+import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import mobi.seacat.client.SeaCatClient;
+import mobi.seacat.client.SeaCatService;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+{
+
+    private TextView resultTextView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("MainActivity", "onCreate!");
+        resultTextView = (TextView) findViewById(R.id.resultTextView);
+        
+        // Enable SeaCat
+        startService(new Intent(getBaseContext(), SeaCatService.class));
 
+        // Start ping timer
+        Timer pingTimer = new Timer();
+        pingTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                PingTimerMethod();
+            }
+
+        }, 0, 1000);
+
+        // Start get timer
+        Timer getTimer = new Timer();
+        getTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try
+                {
+                    GetTimerMethod();
+                }
+
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+
+        }, 0, 1000);
+
+    }
+
+
+    private void PingTimerMethod()
+    {
         try {
-            SeaCatClient.configure(getApplicationContext());
-        }
-        catch (IOException e)
-        {
+            SeaCatClient.ping();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
 
+    private void GetTimerMethod() throws IOException
+    {
+        URL url = new URL("https://testhost.seacat/get/time");
+        HttpURLConnection conn = SeaCatClient.open(url);
+
+        InputStream is = conn.getInputStream();
+        assert(is != null);
+
+
+        String line;
+        String result = "";
+        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        while ((line = rd.readLine()) != null)
+        {
+            result += line;
+        }
+        rd.close();
+        is.close();
+
+        final String output = result;
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                resultTextView.setText(output);
+            }
+        });
+
+    }
+
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
