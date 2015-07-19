247d13bb665e1e718320178949f02941f89701ff
Ales Teska <ales.teska@exidius.com>
2015-07-19 23:17:26 +0100

    Improving CSR implementation

--- client/src/main/java/mobi/seacat/client/CSR.java
+++ client/src/main/java/mobi/seacat/client/CSR.java
@@ -1,100 +1,128 @@
 package mobi.seacat.client;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+
 public class CSR
 {
-    private String state = null;
-    private String country = null;
-    private String locality = null;
-    private String organization = null;
-    private String organizationUnit = null;
-    private String commonName = null;
-    private String title = null;
-    private String surname = null;
-    private String givenName = null;
-    private String email = null;
+    private Map<String, String> paramMap = new HashMap<String, String>();
 
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getLocality() {
-        return locality;
-    }
-
-    public void setLocality(String locality) {
-        this.locality = locality;
-    }
-
-    public String getOrganization() {
-        return organization;
-    }
-
-    public void setOrganization(String organization) {
-        this.organization = organization;
-    }
-
-    public String getOrganizationUnit() {
-        return organizationUnit;
-    }
-
-    public void setOrganizationUnit(String organization_unit) {
-        this.organizationUnit = organization_unit;
-    }
-
-    public String getCommonName() {
-        return commonName;
-    }
-
-    public void setCommonName(String common_name) {
-        this.commonName = common_name;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getGivenName() {
-        return givenName;
-    }
-
-    public void setGivenName(String given_name) {
-        this.givenName = given_name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    ///
 
     CSR()
     {
-
     }
+
+    ///
+
+    public void set(String name, String value)
+    {
+        paramMap.put(name, value);
+    }
+
+    public String get(String name)
+    {
+        return paramMap.get(name);
+    }
+
+    ///
+
+    public String[] toStringArray() {
+        int cnt = paramMap.size();
+        String[] arr = new String[cnt*2];
+
+        int pos = 0;
+        for (Map.Entry<String, String> entry : paramMap.entrySet())
+        {
+            arr[pos++] = entry.getKey();
+            arr[pos++] = entry.getValue();
+
+        }
+
+        return arr;
+    }
+
+    ///
+
+    public String getCountry() {
+        return paramMap.get("C");
+    }
+
+    public void setCountry(String country) {
+        paramMap.put("C", country);
+    }
+
+
+    public String getState() {
+        return paramMap.get("ST");
+    }
+
+    public void setState(String state) {
+        paramMap.put("ST", state);
+    }
+
+
+    public String getLocality() {
+        return paramMap.get("L");
+    }
+
+    public void setLocality(String locality) {
+        paramMap.put("L", locality);
+    }
+
+
+    public String getOrganization() {
+        return paramMap.get("O");
+    }
+
+    public void setOrganization(String organization) {
+        paramMap.put("O", organization);
+    }
+
+
+    public String getOrganizationUnit() {
+        return paramMap.get("OU");
+    }
+
+    public void setOrganizationUnit(String organization_unit) {
+        paramMap.put("OU", organization_unit);
+    }
+
+
+    public String getCommonName() {
+        return paramMap.get("CN");
+    }
+
+    public void setCommonName(String common_name) {
+        paramMap.put("CN", common_name);
+    }
+
+
+    public String getSurname() {
+        return paramMap.get("SN");
+    }
+
+    public void setSurname(String surname) {
+        paramMap.put("SN", surname);
+    }
+
+
+    public String getGivenName() {
+        return paramMap.get("GN");
+    }
+
+    public void setGivenName(String given_name) {
+        paramMap.put("GN", given_name);
+    }
+
+
+    public String getEmailAddress() {
+        return paramMap.get("emailAddress");
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        paramMap.put("emailAddress", emailAddress);
+    }
+
 }
