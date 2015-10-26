5fd0941b67da63361df56e69c21cdd7166bb3b28
Miloslav Pavelka <pavelkamiloslav@gmail.com>
2015-10-26 18:04:58 +0100

    Adding AppId field and enabled/disabled toggle button. When enabled gateway settings is stored to file on local storage.

--- companion/src/main/java/com/teskalabs/seacat/android/companion/MainActivity.java
+++ companion/src/main/java/com/teskalabs/seacat/android/companion/MainActivity.java
@@ -1,45 +1,138 @@
 package com.teskalabs.seacat.android.companion;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.regex.Pattern;
 
 
 public class MainActivity extends ActionBarActivity {
-    public Button buttonSave;
+    public static final String TAG = "compainion.MainActivity";
+    public ToggleButton buttonSave;
     public EditText editTextIP;
     public EditText editTextPort;
+    public EditText editTextAppId;
+
+    private SharedPreferences sharedPref;
+    public static final String PREF_APPID   = "com.teskalabs.seacat.android.companion.PREF_APPID";
+    public static final String PREF_IP      = "com.teskalabs.seacat.android.companion.PREF_IP";
+    public static final String PREF_PORT    = "com.teskalabs.seacat.android.companion.PREF_PORT";
+
+    public Integer STATE_DISABLED       = 0;
+    public Integer STATE_ENABLED        = 1;
+
+    protected String configFileName = "seacat-devel-discover.conf";
+    protected String configFilePath;
 
     private static final Pattern patternIP = Pattern.compile(
             "^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
     private static final Pattern patternPort = Pattern.compile(
             "^\\d+$");
+    private static final Pattern patternAppId = Pattern.compile(
+            "^[a-z1-9A-Z\\-]+(?:(?:\\.)[a-z1-9A-Z\\-]+)+$");
+
+
+    class StateWatcher implements TextWatcher {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) { }
+        @Override
+        public void afterTextChanged(Editable s) {updateStateLayout();}
+    }
+
+
+    protected String getValueIP() { return editTextIP.getText().toString(); }
+    protected String getValuePort() { return editTextPort.getText().toString(); }
+    protected String getValueAppId() { return editTextAppId.getText().toString(); }
+
+    private boolean validateAppId(String appId) { return patternAppId.matcher(appId).matches(); }
+    private boolean validateIP(String ip) { return patternIP.matcher(ip).matches(); }
+    private boolean validatePort(String port) { return patternPort.matcher(port).matches(); }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configFilePath = getFilesDir()+"/"+configFileName;
 
-        buttonSave = (Button) findViewById(R.id.buttonSave);
+        buttonSave = (ToggleButton) findViewById(R.id.buttonSave);
         editTextIP = (EditText) findViewById(R.id.editTextIP);
         editTextPort = (EditText) findViewById(R.id.editTextPort);
+        editTextAppId = (EditText) findViewById(R.id.editTextAppId);
+
+        StateWatcher sWatcher = new StateWatcher();
+        editTextIP.addTextChangedListener(sWatcher);
+        editTextPort.addTextChangedListener(sWatcher);
+        editTextAppId.addTextChangedListener(sWatcher);
+
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        sharedPref = getPreferences(Context.MODE_PRIVATE);
+        editTextIP.setText(sharedPref.getString(PREF_IP, ""));
+        editTextPort.setText(sharedPref.getString(PREF_PORT, ""));
+        editTextAppId.setText(sharedPref.getString(PREF_APPID, ""));
+        updateStateLayout();
+    }
+
+    protected void updateStateLayout()
+    {
+        if (getConfigState() == STATE_ENABLED)
+            buttonSave.setChecked(true);
+        else
+            buttonSave.setChecked(false);
     }
 
     public void buttonSaveOnClick(View v)
     {
-        // Get Input Fields Values
-        String strIP = editTextIP.getText().toString();
-        String strPort = editTextPort.getText().toString();
+        // Temporarily disable button
+        buttonSave.setEnabled(false);
 
+        if (!buttonSave.isChecked())
+        {
+            // Disabling Gateway = remove file
+            File configFile = new File(configFilePath);
+            if (configFile.exists())
+                configFile.delete();
+            Toast.makeText(getApplicationContext(), "Gateway disabled", Toast.LENGTH_SHORT)
+                    .show();
+            buttonSave.setEnabled(true);
+            return;
+        }
+
+        // Get Input Fields Values
+        String strAppId = getValueAppId();
+        String strIP = getValueIP();
+        String strPort = getValuePort();
 
         boolean isValid = true;
+        // Validate App ID
+        if (!validateAppId(strAppId))
+        {
+            isValid = false;
+            editTextAppId.setError("must be a valid app Id.");
+        }
         // Validate IP
         if (!validateIP(strIP))
         {
@@ -54,21 +147,64 @@ public class MainActivity extends ActionBarActivity {
         }
 
         if (!isValid)
+        {
+            buttonSave.setEnabled(true);
+            buttonSave.setChecked(false);
             return;
+        }
 
+        // Write to seacat devel discover config file
+        BufferedWriter out = null;
+        try
+        {
+            FileWriter fstream = new FileWriter(configFilePath);
+            out = new BufferedWriter(fstream);
+            out.write("---\n");
+            out.write(strAppId+"\n");
+            out.write(strIP+"\n");
+            out.write(strPort+"\n");
+            out.close();
+        }
+        catch (IOException e)
+        {
+            Log.e(TAG, "Error: " + e.getMessage());
+            buttonSave.setEnabled(true);
+            buttonSave.setChecked(false);
+            return;
+        }
+
+        // Store to preferences
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(PREF_APPID, strAppId);
+        editor.putString(PREF_IP, strIP);
+        editor.putString(PREF_PORT, strPort);
+        editor.commit();
+
+        // Display message
         Toast.makeText(getApplicationContext(),
-                "IP: "+strIP+"; Port: "+strPort,
-                Toast.LENGTH_LONG)
+                "Gateway enabled."+strPort,
+                Toast.LENGTH_SHORT)
                     .show();
+
+        buttonSave.setEnabled(true);
+        return;
     }
 
-    private boolean validateIP(String ip)
+    public int getConfigState()
     {
-        return patternIP.matcher(ip).matches();
-    }
+        String prefAppId = sharedPref.getString(PREF_APPID, "");
+        String prefIP    = sharedPref.getString(PREF_IP, "");
+        String prefPort  = sharedPref.getString(PREF_PORT, "");
+        File fileConfig = new File(configFilePath);
 
-    private boolean validatePort(String port)
-    {
-        return patternPort.matcher(port).matches();
+        if (!fileConfig.exists())
+            return STATE_DISABLED;
+
+        if (!prefAppId.equals(getValueAppId())
+                || !prefIP.equals(getValueIP())
+                || !prefPort.equals(getValuePort()))
+            return STATE_DISABLED;
+
+        return STATE_ENABLED;
     }
 }
