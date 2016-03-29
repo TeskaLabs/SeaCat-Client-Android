9f450f3378bfbe6f41cee4f6312117edecc45267
Ales Teska <ales.teska@teskalabs.com>
2016-03-29 17:05:36 +0100

    Extending SeaCat return code handling by logging function.

--- client/src/main/java/com/teskalabs/seacat/android/client/util/RC.java
+++ client/src/main/java/com/teskalabs/seacat/android/client/util/RC.java
@@ -1,12 +1,22 @@
 package com.teskalabs.seacat.android.client.util;
 
+import android.util.Log;
+
 import java.io.IOException;
+
+import com.teskalabs.seacat.android.client.SeaCatInternals;
 import com.teskalabs.seacat.android.client.core.seacatcc;
 
 public final class RC
 {
-	public static final void checkAndThrowIOException(String message, int rc) throws IOException
-	{
-		if (rc != seacatcc.RC_OK) throw new IOException(String.format("SeaCat return code %d in %s",rc ,message));
-	}
+    public static final void checkAndThrowIOException(String message, int rc) throws IOException
+    {
+        if (rc != seacatcc.RC_OK) throw new IOException(String.format("SeaCat return code %d in %s",rc ,message));
+    }
+
+    public static final void checkAndLogError(String message, int rc)
+    {
+        if (rc != seacatcc.RC_OK) Log.e(SeaCatInternals.L, String.format("SeaCat return code %d in %s", rc, message));
+    }
+
 }
